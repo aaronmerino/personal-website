31e0527df6805ff00cc380d691b2baf3913370e4
aaron <aaron_merino@outlook.com>
2023-06-21 04:48:29 -0400

    added info in about and contact pages

--- app/about/page.js
+++ app/about/page.js
@@ -1,11 +1,21 @@
+import styles from './page.module.css'
+
 export const metadata = {
   title: 'ABOUT',
 }
 
 // `app/page.js` is the UI for the `/` URL
 export default function Page() {
-  return (<>
-            <h1>ABOUT</h1>
-            <h2>test</h2>
-          </>);
+  return (<div className={styles.description}>
+            <p>
+              Hello, you can call me Aaron.
+            </p>
+            <p>
+              I love studying anything Math, and I also love drawing (which actually involves alot of projective geometry, and my goal is to one day study it), and most of all I love to learn!
+            </p>
+            <p>
+              In this website, I showcase my projects, which include my coding projects, drawings, and any cool math stuff I've learned!
+            </p>
+
+          </div>);
 }
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,11 +1,19 @@
+import styles from './page.module.css'
+import Link from 'next/link';
+
 export const metadata = {
   title: 'CONTACT',
 }
 
 // `app/page.js` is the UI for the `/` URL
 export default function Page() {
-  return (<>
-            <h1>CONTACT</h1>
-            <h2>test</h2>
-          </>);
+  return (<div className={styles.description}>
+            <p>
+              <b>email</b>: aaronmerino00@gmail.com
+            </p>
+            <p>
+              <b>instagram</b>: { }
+              <Link href={'https://www.instagram.com/rfalke01/'}>link</Link>
+            </p>
+          </div>);
 }
