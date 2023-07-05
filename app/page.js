28b364db6ba75fce0b85a4c1ee3a0f9bf3dcaa18
aaron <aaron_merino@outlook.com>
2023-07-05 16:49:47 -0400

    made image larger for smaller devices

--- app/page.js
+++ app/page.js
@@ -10,8 +10,6 @@ export const metadata = {
 export default async function Page() {
   const posts = await getSortedPostsData();
   return (<div className={styles.main}>
-            <h2>UNDER CONSTRUCTION! PLEASE WAIT WHILE I GATHER MY BRAIN BITS!</h2>
-            
             {posts.map((post) => {
               return <Post key={post.title} title={post.title} date={post.date} images={post.images} content={post.content}></Post>
             })}
