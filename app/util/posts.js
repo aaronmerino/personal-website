2f5b87b0b2eca78f37d937444e44bdae0e05b9ea
aaron <aaron_merino@outlook.com>
2023-07-11 13:16:39 -0400

    added comments about the getSortedPostsData function

--- app/util/posts.js
+++ app/util/posts.js
@@ -4,6 +4,14 @@ import matter from 'gray-matter';
 
 const postsDirectory = path.join(process.cwd(), 'public', 'posts');
 
+/**
+ * This function will parse the public/post's directory and generate a sorted list of objects 
+ * that contains various information of each post. Each post in the directory contains
+ * an image folder and a Markdown file that contains a description of each image. The list is sorted 
+ * based on date indicated in the Markdown file.
+ * 
+ * @return {[objects]} [list of objects representing each post in the directory]
+ */
 export async function getSortedPostsData() {
   try {
     const folders = await fsPromises.readdir(postsDirectory);
